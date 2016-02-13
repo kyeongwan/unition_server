0e7f57c9914b98e25e0360a0b0b61be88ccd9103
choeunsun <choeunsun@hanmail.net>
2016-02-13 20:12:37 +0900

    m

--- api/models/User.js
+++ api/models/User.js
@@ -56,26 +56,26 @@ module.exports = {
   		phoneNumber: inputs.phoneNumber
   	}).then(function(user){
   		var gcmReg = user.gcmReg;
-  		//var gcm = require('node-gcm');
-		//var message = new gcm.Message();
+  		var gcm = require('node-gcm');
+		var message = new gcm.Message();
 
-		// var message = new gcm.Message({
-	 //    	collapseKey: 'demo',
-	 //    	delayWhileIdle: true,
-	 //    	timeToLive: 3,
-	 //    	data: {
-	 //        	key1: '안녕하세요.',
-  //       		key2: 'saltfactory push demo'
-  //   		}
-		// });
+		var message = new gcm.Message({
+	    	collapseKey: 'demo',
+	    	delayWhileIdle: true,
+	    	timeToLive: 3,
+	    	data: {
+	        	key1: '안녕하세요.',
+        		key2: 'saltfactory push demo'
+    		}
+		});
 
-		// var server_access_key = '푸시 프로바이더 서버 access key 값';
-		// var sender = new gcm.Sender(server_access_key);
-		// var registrationIds = [];
+		var server_access_key = '푸시 프로바이더 서버 access key 값';
+		var sender = new gcm.Sender(server_access_key);
+		var registrationIds = [];
 
-		// var registration_id = '안드로이드 registration_id 값';
-		// // At least one required
-		// registrationIds.push(registration_id);
+		var registration_id = '안드로이드 registration_id 값';
+		// At least one required
+		registrationIds.push(registration_id);
 
 		/**
 		 * Params: message-literal, registrationIds-array, No. of retries, callback-function
