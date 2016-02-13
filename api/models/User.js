4e1af988c9f4a160d5324a5fadacb05f80d6fcc5
choeunsun <choeunsun@hanmail.net>
2016-02-13 17:34:14 +0900

    유저 스키마 추가

---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+	id:{
+		type: 'string',
+		required: true
+	},
+	phoneNumber: {
+		type: 'string',
+		required: true
+	},
+	nToken: {
+		type: 'string',
+		required: true
+	},
+	gcmReg: {
+		type: 'string',
+		required: true
+	}
+  }
+  
+};
+
